b8cb85d8d8ce8bc3fb04e5a1a13df8a6c4d6ab38
Dan Roizer <Danny159@users.noreply.github.com>
2024-12-19 22:34:43 +0000

    Add version and API endpoint configuration to Codeplx settings

--- config/codeplx.php
+++ config/codeplx.php
@@ -1,5 +1,11 @@
 <?php
 return [
+    // Version
+    'version' => '1.0.0',
+
+    // The API key for the Codeplx Support API
     'api_key' => env('CODEPLX_API_KEY'),
+
+    // The endpoint for the Codeplx Support API
     'endpoint' => env('CODEPLX_ENDPOINT', 'https://support.test/api'),
 ];
