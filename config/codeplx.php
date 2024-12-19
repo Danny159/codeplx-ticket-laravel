be687e097c5a3844e91a5e478fd821980642b418
Dan Roizer <Danny159@users.noreply.github.com>
2024-12-19 23:34:55 +0000

    Fix Codeplx API endpoint URL by removing trailing slash

--- config/codeplx.php
+++ config/codeplx.php
@@ -7,5 +7,5 @@ return [
     'api_key' => env('CODEPLX_API_KEY'),
 
     // The endpoint for the Codeplx Support API
-    'endpoint' => env('CODEPLX_ENDPOINT', 'https://support.codeplx.com/'),
+    'endpoint' => env('CODEPLX_ENDPOINT', 'https://support.codeplx.com'),
 ];
