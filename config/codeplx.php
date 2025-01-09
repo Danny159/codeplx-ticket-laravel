9a8512877bfbbda6e41d573ad8f9db8e9ca16c09
Dan Roizer <Danny159@users.noreply.github.com>
2025-01-09 12:06:11 +0000

    Bump version to 1.1.1 in Codeplx configuration

--- config/codeplx.php
+++ config/codeplx.php
@@ -1,11 +1,8 @@
 <?php
 return [
     // Version
-    'version' => '1.1.0',
+    'version' => '1.1.1',
 
     // The API key for the Codeplx Support API
     'api_key' => env('CODEPLX_API_KEY'),
-
-    // The endpoint for the Codeplx Support API
-    'endpoint' => env('CODEPLX_ENDPOINT', 'https://support.codeplx.com'),
 ];
